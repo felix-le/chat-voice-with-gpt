a7717b76e15648afd6f6db56759038732674ef35
felixelement <felix@element6.io>
2023-10-17 00:46:17 -0400

    finished add  tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import HelloWorld from "./src/components/HelloWorld";
 export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center">
       <Text className="text-red-500">hello world223!</Text>
       <StatusBar style="auto" />
+
+      <HelloWorld />
     </View>
   );
 }
--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,20 +1,8 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const HelloWorld: React.FC = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
-  );
+  return <Text className="text-blue-500">Hello World from component!</Text>;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default HelloWorld;
